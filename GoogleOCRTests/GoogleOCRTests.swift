caf915b04453dd7e70b1269592c99f8b3bbad0dc
mac <MT@mac.local>
2017-03-09 23:03:21 +0300

    Initial Commit

---
+++ GoogleOCRTests/GoogleOCRTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoogleOCRTests.swift
+//  GoogleOCRTests
+//
+//  Created by Muna Tayeb on 6/10/1438 AH.
+//  Copyright © 1438 Muna Tayeb. All rights reserved.
+//
+
+import XCTest
+@testable import GoogleOCR
+
+class GoogleOCRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
